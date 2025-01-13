6ff7f09050b0a95a64a033c5ad1f83505d5a5ad8
cortinezO98 <jcortinezosorio@gmail.com>
2025-01-13 16:46:50 -0500

    config de google cloud add en views

--- search/myapp/views.py
+++ search/myapp/views.py
@@ -10,14 +10,14 @@ import datetime
 def index(request):
     return render(request, 'index.html')
 
-def get_auth_token(request):
-    payload = {
-        "iss": "http://31735205742-l72g558ibfdeuvmaht5lrt56fr9d0ra6.apps.googleusercontent.com/",
-        "sub": "nelson.gil@iq-online.com",
-        "iat": datetime.datetime.utcnow(),
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-        "aud": "https://cloud.google.com/ai/gen-app-builder"
-    }
-    secret_key = settings.SECRET_KEY
-    token = jwt.encode(payload, secret_key, algorithm="HS256")
-    return JsonResponse({"token": token})
+#def get_auth_token(request):
+    #payload = {
+    #    "iss": "http://31735205742-l72g558ibfdeuvmaht5lrt56fr9d0ra6.apps.googleusercontent.com/",
+    #    "sub": "nelson.gil@iq-online.com",
+    #    "iat": datetime.datetime.utcnow(),
+    #     "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+    #    "aud": "https://cloud.google.com/ai/gen-app-builder"
+    #}
+    #@secret_key = settings.SECRET_KEY
+    #token = jwt.encode(payload, secret_key, algorithm="HS256")
+    #return JsonResponse({"token": token})
